d12dd01b9fc41424d1fba791ab588a854734ebf1
Murilo <murilo.ti32@outlook.com>
2023-10-06 17:11:07 -0300

    Create app2.js

---
+++ app2.js
@@ -0,0 +1,7 @@
+//Numeric separator
+let nb = 1_000 // equivalent to 1000
+let nb2 = 1_000_000 // equivalent 1000000(1 milion)
+
+//Converting numeric for string
+let answer = 42;
+answaer.toString(); //"42"
