8e6387cdca166c9e760e5cf206e139238181d6ab
Murilo <murilo.ti32@outlook.com>
2023-10-10 17:40:09 -0300

    Update app2.js

--- app2.js
+++ app2.js
@@ -5,3 +5,32 @@ let nb2 = 1_000_000 // equivalent 1000000(1 milion)
 //Converting numeric for string
 let answer = 42;
 answaer.toString(); //"42"
+
+//10/10/2023 Converting string in number
+let str = "44"
+Number.parseInt(str,10); //Return 44
+
+//Operators
+// * multiply values
+// % rest of division
+
+let number1 = 5 / 6
+let number2 = 5 % 6
+
+
+//Condition 'if'  in JS
+if (condition) {
+    //do something
+}
+
+//Exemplo of use
+let grade = 15;
+
+if (grade > 10) {
+    console.log("Passing grade");
+} else if (grade = 10) {
+    console.log("Passing on the limit");
+} else {
+    console.log("Failing grade");
+}
+
