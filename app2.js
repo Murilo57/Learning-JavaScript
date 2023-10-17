99343f0ecb36304a4bf9094b682538399e3a9368
Murilo <murilo.ti32@outlook.com>
2023-10-17 17:13:43 -0300

    Exercicie of conditions boolean

--- app2.js
+++ app2.js
@@ -34,3 +34,48 @@ if (grade > 10) {
     console.log("Failing grade");
 }
 
+// advanced if
+function canVote(age) {
+    if (age >= 18) {
+      return  true;
+    }
+    return false;
+}
+
+//17/10/2023 Return boolean
+
+function isPassing(grade) {
+    if (grade >= 19) {
+        return true;
+    }
+    return false;
+}
+
+//or 
+
+function isPassing(grade) {
+    return grade >= 19
+}
+
+isPassing(12)
+//Even % odd
+
+//Even numbers
+4% 2 //0 
+6% 2 //0
+8% 2 //0
+10% 2 //0
+
+//Odd numbers
+3% 2 //1
+5% 2 //1
+7% 2 //1
+9% 2 //1
+
+//Notice how the remainder of dividing by two is always 0 when the number is even. Whereas it is 1 when the number is odd.
+
+//Observe como o resto da divisão por dois é sempre 0 quando o número é par . Considerando que é 1 quando o número é ímpar .
+
+
+
+
